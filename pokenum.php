99363b987908849471767b9e2e3189129ab671ea
Steeve Lennmark <steevel@handeldsbanken.se>
2011-02-13 16:18:04 +0100

    Cleaned up pokenum example file

--- pokenum.php
+++ pokenum.php
@@ -1,9 +1,10 @@
 <?php
+/**
+ * A couple of pokenum examples
+ */
 var_dump(pokenum(POKENUM_TEXAS, array(array('Ah', 'Ad'), array('Kd', 'Kc'), array('Qc', 'Qd'))));
-/*
 var_dump(pokenum(POKENUM_OMAHA, array(array('Ad', 'Ac', 'Ts', 'Js'), array('8d', '7d', '6c', '5c'))));
 var_dump(pokenum(POKENUM_TEXAS, array(array('Ah', 'Ad'), array('Kd', 'Kc')), array('5d', '5c', '5h')));
-*/
 
 var_dump(pokenum(POKENUM_TEXAS,
 	array(
@@ -21,6 +22,9 @@ var_dump(pokenum(POKENUM_TEXAS,
 	array('4d', 'Kc')
 ));
 
-echo "\n--------------------------------------\n\n";
+echo "\n--------------------------------------\n";
+echo "ERROR HANDLING";
+echo "\n--------------------------------------\n";
+
 var_dump(pokenum(POKENUM_TEXAS, array(array('Ac', 'Ac'), array('Kh', 'Kd'))));
 var_dump(pokenum_error());

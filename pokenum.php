498db96184f8c9ade5a77a1385cfd615c3a644ce
Steeve Lennmark <steevel@handeldsbanken.se>
2011-02-18 12:43:22 +0100

    Rewrote the pokenum.php example file to work exactly as the pokenum binary.

--- pokenum.php
+++ pokenum.php
@@ -1,30 +1,30 @@
 <?php
-/**
- * A couple of pokenum examples
- */
-var_dump(pokenum(PN_TEXAS, array(array('Ah', 'Ad'), array('Kd', 'Kc'), array('Qc', 'Qd'))));
-var_dump(pokenum(PN_OMAHA, array(array('Ad', 'Ac', 'Ts', 'Js'), array('8d', '7d', '6c', '5c'))));
-var_dump(pokenum(PN_TEXAS, array(array('Ah', 'Ad'), array('Kd', 'Kc')), array('5d', '5c', '5h')));
+/*
+$ pokenum -mc 100000 -h Ah Kh - 9c 9d -- Jh Th 2c
+Holdem Hi: 100000 sampled boards containing 2c Jh Th
+cards     win   %win      lose  %lose       tie   %tie        EV
+Ah Kh   63325  63.33     36675  36.67         0   0.00     0.633
+9c 9d   36675  36.67     63325  63.33         0   0.00     0.367
+*/
+$result = pokenum(PN_TEXAS, array('Ah Kh', '9c 9d'), 'Jh Th 2c');
+if (false === $result) {
+	printf("POKENUM_ERROR: (%d): %s\n", pokenum_errno(), pokenum_error());
+	exit;
+}
 
-var_dump(pokenum(PN_TEXAS,
-	array(
-		array('Ac', 'Ad'),
-		array('Kh', 'Kd')
-	),
-	array('5d', 'Td', '2d')
-));
-var_dump(pokenum(PN_TEXAS,
-	array(
-		array('Ac', 'Ad'),
-		array('Kh', 'Kd')
-	),
-	array('5d', 'Td', '2d'),
-	array('4d', 'Kc')
-));
-
-echo "\n--------------------------------------\n";
-echo "ERROR HANDLING";
-echo "\n--------------------------------------\n";
-
-var_dump(pokenum(PN_TEXAS, array(array('Ac', 'Ac'), array('Kh', 'Kd'))));
-var_dump(pokenum_error());
+printf("%s: %d sampled boards containing %s\n",
+		$result['name'],
+		$result['iterations'],
+		implode(' ', $result['board']));
+echo "cards     win   %win      lose  %lose       tie   %tie        EV\n";
+foreach ($result['hands'] as $hand) {
+	printf("%s %7d %6.2f   %7d %6.2f   %7d %6.2f     %5.3f\n",
+			implode(' ', $hand['hand']),
+			$hand['win'],
+			$hand['win'] / $result['iterations'] * 100,
+			$hand['lose'],
+			$hand['lose'] / $result['iterations'] * 100,
+			$hand['tie'],
+			$hand['tie'] / $result['iterations'] * 100,
+			$hand['ev']);
+}
